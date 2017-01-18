a95aa7e92e1b9d81979c8cfb9fda3e3058a39e82
sakorkko <samuli.korkko@gmail.com>
2017-01-18 12:57:06 +0200

    linking map and list to start

--- PingMe/app/src/main/java/com/example/pingme/StartPage.java
+++ PingMe/app/src/main/java/com/example/pingme/StartPage.java
@@ -1,7 +1,9 @@
 package com.example.pingme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartPage extends AppCompatActivity {
 
@@ -10,4 +12,9 @@ public class StartPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
     }
+
+    public void openList(View v){
+        startActivity(new Intent(StartPage.this, ListView.class));
+    }
+    public void openMap(View v) { startActivity(new Intent(StartPage.this, MapsActivity.class)); }
 }
