79552d0313d5a28b2e91e266bdb2d95489b1dee0
kannadan <joonas.sou@gmail.com>
2017-01-24 16:20:11 +0200

    jotain

--- PingMe/app/src/main/java/com/example/pingme/ListView.java
+++ PingMe/app/src/main/java/com/example/pingme/ListView.java
@@ -1,16 +1,23 @@
 package com.example.pingme;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
-public class ListView extends AppCompatActivity {
+public class ListView extends  ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+        String[] list = {"Wash my dog - John", "Walk my car - Dinglerberg"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getListView().getContext(),
+                android.R.layout.simple_list_item_1, list);
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        getListView().setAdapter(adapter);
     }
 
     public void openMap(View v) { startActivity(new Intent(ListView.this, MapsActivity.class)); }
